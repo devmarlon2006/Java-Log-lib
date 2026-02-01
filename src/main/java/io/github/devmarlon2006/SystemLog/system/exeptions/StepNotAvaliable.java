df688dfc74fad7d220f32549ef5b7bda5721661f
devmarlon2006 <mo14421890@gmail.com>
2026-02-01 16:53:46 -0300

    A StepNotAvaliable exception class

---
+++ src/main/java/io/github/devmarlon2006/SystemLog/system/exeptions/StepNotAvaliable.java
@@ -0,0 +1,7 @@
+package io.github.devmarlon2006.SystemLog.system.exeptions;
+
+public class StepNotAvaliable extends Exception {
+    public StepNotAvaliable(String message) {
+        super(message);
+    }
+}
