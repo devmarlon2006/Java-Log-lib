2597260e561b8bd33473f7328575258f49d19183
devmarlon2006 <mo14421890@gmail.com>
2026-02-12 21:15:59 -0300

    Update na interface para padronização da criação do log

--- src/main/java/io/github/devmarlon2006/SystemLog/system/Logablee.java
+++ src/main/java/io/github/devmarlon2006/SystemLog/system/Logablee.java
@@ -5,33 +5,28 @@ import io.github.devmarlon2006.SystemLog.log.models.SystemLog;
 
 public interface Logablee {
 
+
     /**
      * Methods para o log de sistema;
      */
 
     SystemLog provinderLogInfo(String message);
 
-
-    /**
-     *
-     * public void catchLogInfo() {
-     *     this.youLog = logInfo();
-     * }
-     */
-    void catchLogInfo();
-
-
-    default  LogHandle logInfo(String message) {
+    default LogHandle initializeLogInfo(String message) {
         return LogHandle.of().configure((logParams) -> logParams
                 .addId()
                 .statusCode(200)
                 .addMessage(message)
-                .addSteps(null)
         );
     }
 
-    default LogHandle logError(String message) {
-        SystemLog cretedLogError = new LogHandle().configure( (logParams) -> logParams ).build();
-        return null;
+    LogHandle provinderLogError(String message);
+
+    default LogHandle initializeLogError(String message) {
+        return LogHandle.of().configure( (logParams) -> logParams
+                .addId()
+                .statusCode(500)
+                .addMessage(message)
+        );
     }
 }
