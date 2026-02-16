d3e4d9079389c693aa528387f56f7951f72916f0
devmarlon2006 <mo14421890@gmail.com>
2026-02-16 20:08:10 -0300

    Melhorias nos métodos de manipulação e obtenção de collections dentro da classe `LogHandle`

--- src/main/java/io/github/devmarlon2006/SystemLog/log/LogHandle.java
+++ src/main/java/io/github/devmarlon2006/SystemLog/log/LogHandle.java
@@ -7,31 +7,38 @@ import io.github.devmarlon2006.SystemLog.system.exeptions.StepNotAvaliable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 public class LogHandle extends Handle implements Buildable<SystemLog , LogHandle> {
 
-    private UUID LOG_ID;
-    private int SUPER_STATUS_CODE;
-    private String MESSAGE;
-    private List<Steps> LOG_STEPS = new ArrayList<>();
-    private List<StepsHandle> stepsHandlesBuffer = new ArrayList<>();
+    private UUID LOG_ID;                                          // (gerado automaticamente) exigido
+    private int SUPER_STATUS_CODE;                                // (use a interface 'Logable' ou defina você mesmo) exigido
+    private String MESSAGE;                                       // opcional mas recomendado
+    private final List<Steps> LOG_STEPS = new ArrayList<>();      // exigido pois <Steps> é uma classe imutavel
+    private List<StepsHandle> STEPS_HANDLES = new ArrayList<>();  // exixido para criar os steps apartir de <StepsHandle>
 
     public LogHandle () {
     }
 
-    public LogHandle(UUID logId, String message, List<Steps> steps) {
+    public LogHandle(UUID logId, String message) {
         LOG_ID = logId;
         this.MESSAGE = message;
-        this.LOG_STEPS = steps;
     }
 
     public static LogHandle of() {
         return new LogHandle();
     }
 
+    public boolean isError() {
+        return this.SUPER_STATUS_CODE >= 400;
+    }
+
+    public boolean isSuccess() {
+        return !this.isError();
+    }
 
     /**
      * Implementation of Buildable
@@ -39,7 +46,7 @@ public class LogHandle extends Handle implements Buildable<SystemLog , LogHandle
 
     @Override
     public SystemLog build() {
-        return SystemLog.of(this);
+        return SystemLog.from(this);
     }
 
     @Override
@@ -68,12 +75,8 @@ public class LogHandle extends Handle implements Buildable<SystemLog , LogHandle
         return (this);
     }
 
-    public LogHandle addSteps(StepsHandle addSteps){
-        this.stepsHandlesBuffer.add(addSteps);
-        return (this);
-    }
-
-    public LogHandle addTrace() {
+    public LogHandle addStepHandle(StepsHandle addSteps){
+        this.STEPS_HANDLES.add(addSteps);
         return (this);
     }
 
@@ -89,61 +92,71 @@ public class LogHandle extends Handle implements Buildable<SystemLog , LogHandle
         return this.MESSAGE;
     }
 
-    public List<Steps> obtainLogSteps() throws NullPointerException {
-        if (this.LOG_STEPS.isEmpty()) {
-            throw new NullPointerException("Steps not avaliable");
-        }
-        return this.LOG_STEPS;
-    }
-
-    //------
-
     public int getStatusCode() {
         return this.SUPER_STATUS_CODE;
     }
 
-    public LogHandle buildSteps() {
-        if (this.stepsHandlesBuffer != null) {
-            for (StepsHandle step : this.stepsHandlesBuffer) {
-                assert false;
+    public Optional<List<Steps>> obtainLogSteps() throws NullPointerException {
+        return this.LOG_STEPS.isEmpty() ? Optional.empty() : Optional.of(this.LOG_STEPS);
+    }
+
+    public Optional<List<StepsHandle>> obtainLogStepsHandle() {
+        return Optional.of(this.STEPS_HANDLES);
+    }
+
+    /**
+     * StepsHandle collection manipulation methods
+     */
+
+    public List<StepsHandle> filterStepsHandleAndGet(String filter , StepsHandle.Camps camp) {
+        Predicate<StepsHandle> condition = (step) -> step.getCamps(camp).contains(filter);
+        return this.STEPS_HANDLES.stream().filter(condition).toList();
+    }
+
+    public List<StepsHandle> filterStepsHandleAndSet(String filter , StepsHandle.Camps camp) {
+        this.STEPS_HANDLES = this.filterStepsHandleAndGet(filter, camp);
+        return this.STEPS_HANDLES;
+    }
+
+    public StepsHandle getIndividualStepHandle(int index) throws StepNotAvaliable {
+        if (index > this.STEPS_HANDLES.size() || this.hasStepsHandle()) {
+            throw new StepNotAvaliable("This step is not avaliable -> " + index);
+        }
+        return this.STEPS_HANDLES.get(index);
+    }
+
+    public boolean hasStepsHandle() {
+        return !this.STEPS_HANDLES.isEmpty();
+    }
+
+
+    /**
+     * Steps collection manipulation methods
+     */
+
+    public LogHandle buildSteps() throws StepNotAvaliable {
+        if (this.hasStepsHandle()) {
+            for (StepsHandle step : this.STEPS_HANDLES) {
                 this.LOG_STEPS.add(step.build());
             }
+        }else {
+            throw new StepNotAvaliable("Não existe passos para serem criados");
         }
         return this;
     }
 
-    public List<StepsHandle> filterStepsAndSet(String filter , StepsHandle.Camps camp) {
-        Predicate<StepsHandle> condition = (step) -> step.getCamps(camp).contains(filter);
-        this.stepsHandlesBuffer = this.stepsHandlesBuffer.stream().filter(condition).toList();
-        return this.stepsHandlesBuffer;
-    }
-
-    public List<StepsHandle> filterStepsAndGet(String filter , StepsHandle.Camps camp) {
-        Predicate<StepsHandle> condition = (step) -> step.getCamps(camp).contains(filter);
-        return this.stepsHandlesBuffer.stream().filter(condition).toList();
-    }
-
-    public StepsHandle getIndividualStep(int index) throws StepNotAvaliable {
-        if (index > this.stepsHandlesBuffer.size()) {
-            throw new StepNotAvaliable("This step is not avaliable -> " + index);
-        }
-        return this.stepsHandlesBuffer.get(index);
+    public void transformStepsAndBuild(List<StepsHandle> handleSteps) {
+        this.STEPS_HANDLES = handleSteps;
+        this.configure((S) -> {
+            try {
+                return S.buildSteps();
+            } catch (StepNotAvaliable e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public boolean hasSteps() {
         return !this.LOG_STEPS.isEmpty();
     }
-
-    public boolean hasStepsBuffer() {
-        return !this.stepsHandlesBuffer.isEmpty();
-    }
-
-    public boolean isError() {
-        return this.SUPER_STATUS_CODE >= 400;
-    }
-
-    public boolean isSuccess() {
-        return !this.isError();
-    }
-
 }
