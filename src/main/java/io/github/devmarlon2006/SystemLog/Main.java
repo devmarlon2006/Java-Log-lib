3f69c8cd9f33942315f56457bc539a7a4a037538
devmarlon2006 <mo14421890@gmail.com>
2026-02-16 20:09:32 -0300

    Nenhuma modificação relevante

--- src/main/java/io/github/devmarlon2006/SystemLog/Main.java
+++ src/main/java/io/github/devmarlon2006/SystemLog/Main.java
@@ -1,10 +1,9 @@
 package io.github.devmarlon2006.SystemLog;
 
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Log is working");
-    }
-
-    private void teste() {
+        System.out.println("Hello Log!");
     }
 }
